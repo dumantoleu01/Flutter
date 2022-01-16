760bbcd1193b998be6fcd447eee9e2acbcde434b
dumantoleu01 <57158643+dumantoleu01@users.noreply.github.com>
2022-01-17 03:53:59 +0600

    Delete homebank directory

--- homebank/android/app/src/main/kotlin/com/example/scan_and_generate_qr_code/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scan_and_generate_qr_code
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- homebank/lib/constants.dart
+++
@@ -1,3 +0,0 @@
-final urlLogin = "https://ge.buginsoft.kz/api/v1/auth/login";
-
-final urlProduct = "https://ge.buginsoft.kz//api/v1/products/product";
